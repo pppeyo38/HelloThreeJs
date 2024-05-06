37ae96c2c10f19a0f74a7b784fbd5ced2ba3bdce
pppeyo38 <fuwafuwa.melon47@gmail.com>
2024-05-06 23:07:19 +0900

    section 11 done

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,23 @@ import GUI from "lil-gui";
 let scene: THREE.Scene,
   camera: THREE.PerspectiveCamera,
   renderer: THREE.WebGLRenderer,
+  particlesGeometry: THREE.BufferGeometry<THREE.NormalBufferAttributes>,
   controls: OrbitControls,
   clock: THREE.Clock;
 
-window.addEventListener("load", init);
+const count = 10000;
 
-//サイズ
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+window.addEventListener("load", init);
 
 function init() {
   // UIデバッグ
   const gui = new GUI();
+
+  //サイズ
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
 
   // シーンの追加
   scene = new THREE.Scene();
@@ -39,17 +42,53 @@ function init() {
   document.body.appendChild(renderer.domElement);
 
   /**
- * テクスチャ設定
- * /
+   * テクスチャ設定
+   */
+  const textureLoader = new THREE.TextureLoader();
+  const particlesTexture = textureLoader.load(
+    "../src/textures/particles/8.png"
+  );
 
+  /**
+   * パーティクル作成
+   */
+  particlesGeometry = new THREE.BufferGeometry();
 
-/**
- * パーティクル作成
- */
+  const positionArray = new Float32Array(count * 3);
+  const colorArray = new Float32Array(count * 3);
+  for (let i = 0; i < count * 3; i++) {
+    positionArray[i] = (Math.random() - 0.5) * 10;
+    colorArray[i] = Math.random();
+  }
+
+  particlesGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positionArray, 3)
+  );
+  particlesGeometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(colorArray, 3)
+  );
+
+  const pointMaterial = new THREE.PointsMaterial({
+    size: 0.15,
+    transparent: true,
+    alphaMap: particlesTexture,
+    // alphaTest: 0.001,
+    // depthTest: false,
+    depthWrite: false,
+    vertexColors: true,
+    blending: THREE.AdditiveBlending,
+  });
+
+  const particles = new THREE.Points(particlesGeometry, pointMaterial);
+  scene.add(particles);
 
   // マウス操作
   controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
+
+  clock = new THREE.Clock();
 
   window.addEventListener("resize", onWindowResize);
 
@@ -70,6 +109,17 @@ function animate() {
   const elapsedTime = clock.getElapsedTime();
 
   controls.update();
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+
+    const positionX = particlesGeometry.attributes.position.array[i3];
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + positionX
+    );
+  }
+
+  particlesGeometry.attributes.position.needsUpdate = true;
 
   //レンダリング
   renderer.render(scene, camera);
